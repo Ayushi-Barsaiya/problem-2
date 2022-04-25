02ffaf0657cd297edcc2f4de6591d8916ae04f77
Ayushi-Barsaiya <100615114+Ayushi-Barsaiya@users.noreply.github.com>
2022-04-26 00:36:43 +0530

    problem statement completed

--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -1,46 +1,47 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 const DataFetch = () => {
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([]);
+  const ClickHandler = () => {
 
-    const ClickHandler = () => {
-        const url = `https://swapi.dev/api/people/${Math.floor(Math.random() * 10) + 1}`
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const json = await response.json();
-                console.log(json);
-                setPosts([...posts,
-                   json.name])
+    const url = `https://swapi.dev/api/people/${Math.floor(Math.random() * 10) + 1}`
+
+    const fetchData = async () => {
+      try {
+            const response = await fetch(url);
+            const json = await response.json();
+                //console.log(json);
+            setPosts([...posts, json.name]);
+        } catch (error){
+            console.log("Unexpected error occured");
             }
-            catch (error) {
-                console.log("error");
-            }
-        };
-        fetchData();
-    }
+    };
+    fetchData();
+  };
 
-    const DeleteHandler = () => {
-            
-    }
+    const DeleteHandler = (e) => {
+        const ele = document.getElementsByClassName(e)[0];
+        ele.remove();
+    };
 
-    return (
-        <div>
-            <div>
-                <button onClick={ClickHandler}>ADD RECORD</button>
-            </div>
-            <table>
-                {posts.map((post) =>
-                    <tr>
-                        <td key={Math.random()}>{post}</td>
-                        <td><button onClick={DeleteHandler}>delete</button></td>
-                    </tr>
-                )}
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <button onClick={ClickHandler}>ADD RECORD</button>
+      </div>
+      <table>
+        {posts.map((post) => (
+          <tr className={post}>
+            <td key={Math.random()}>{post}</td>
+            <td>
+              <button name={post} onClick={() => DeleteHandler(post)}> delete </button>   
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 };
 
-export default DataFetch
+export default DataFetch;
