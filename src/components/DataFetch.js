500261a43e62763bbeaec8f5cd986347a7429c7a
Ayushi-Barsaiya <100615114+Ayushi-Barsaiya@users.noreply.github.com>
2022-04-28 13:26:08 +0530

    css done

--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -1,41 +1,40 @@
 import React, { useState } from "react";
-
+import '../App.css';
 const DataFetch = () => {
+
   const [posts, setPosts] = useState([]);
 
   const ClickHandler = () => {
-
     const url = `https://swapi.dev/api/people/${Math.floor(Math.random() * 10) + 1}`
-
     const fetchData = async () => {
       try {
-            const response = await fetch(url);
-            const json = await response.json();
-                //console.log(json);
-            setPosts([...posts, json.name]);
-        } catch (error){
-            console.log("Unexpected error occured");
-            }
+        const response = await fetch(url);
+        const json = await response.json();  //console.log(json);
+        setPosts([...posts, json.name]);
+      } catch (error) {
+        console.log("Unexpected error occured");
+      }
     };
     fetchData();
   };
 
-    const DeleteHandler = (e) => {
-        const ele = document.getElementsByClassName(e)[0];
-        ele.remove();
-    };
+  const DeleteHandler = (e) => {
+    const ele = document.getElementsByClassName(e)[0];
+    ele.remove();
+  };
 
   return (
     <div>
       <div>
-        <button onClick={ClickHandler}>ADD RECORD</button>
+        <button className="button" onClick={ClickHandler}>Add Record</button>
       </div>
-      <table>
+      <table id="table">
+        <tr><td>Name</td><td></td></tr>
         {posts.map((post) => (
           <tr className={post}>
             <td key={Math.random()}>{post}</td>
-            <td>
-              <button name={post} onClick={() => DeleteHandler(post)}> delete </button>   
+            <td id="button_td">
+              <button className="button" name={post} onClick={() => DeleteHandler(post)}> Delete </button>
             </td>
           </tr>
         ))}
